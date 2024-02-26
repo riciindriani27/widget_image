927cd549f389d419755309d6815f50cb1ea025fc
riciindriani27 <riciindriani83@gmail.com>
2024-02-26 16:24:01 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/widgetimage/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.widgetimage
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+  return MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("Image Widget Flutter"),
+        backgroundColor: Colors.blue[400],
+      ),
+      body: Center(
+        child: Container(
+          width: 400,
+          height: 200,
+          color: Colors.blue[200],
+          // child: Image.asset("images/xirpl1.jpeg",
+          // fit: BoxFit.fill,),
+          child: Image.network("https://picsum.photos/200/300",
+          fit: BoxFit.fill,),
+        ),
+      ),
+    ),
+  );
+  }
+}
+
